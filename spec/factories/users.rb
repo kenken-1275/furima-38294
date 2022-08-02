63b92d5647abf7673e5c68eae236cd5e7f87801a
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-02 22:10:23 +0900

    Fakerパスワード生成の変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.initials(number:3)}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length:6, mix_case:false)}
+    password {"7a" + Faker::Internet.password(min_length:6, mix_case:false)}
     password_confirmation {password}
     chinese_last_name {Faker::Name.last_name}
     chinese_first_name {Faker::Name.first_name}
