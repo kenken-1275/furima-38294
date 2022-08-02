e502201c2fce8071256d8245cd1161dc0f88f57f
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-02 14:37:10 +0900

    ユーザーモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname , presence: true
+  validates :chinese_last_name , presence: true
+  validates :chinese_first_name , presence: true
+  validates :japanese_last_name , presence: true
+  validates :japanese_first_name , presence: true
+  validates :date_of_birth , presence: true
+
+  has_many :items
+  has_mane :dealings
+
 end
