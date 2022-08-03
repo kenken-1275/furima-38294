6fcfbcc78699516d94e11315b228084a6a981696
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-03 16:47:13 +0900

    itemsコントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,32 @@
 class ItemsController < ApplicationController
 
+  before_action :move_to_index,only:[:new]
+
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name,:explanation,:price,:category_id,:condition_id,:postage_id,:area_id,:arrival_time_id,:image).merge(user_id:current_user.id)
+  end
+
 end
