ba708947cf726c9ab5aa1e2db2b091e4ef7f9cb8
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-04 19:17:22 +0900

    itemsコントローラのdestroyアクション変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit,]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_params, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -35,10 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    unless @item.user.id != current_user.id
-    @item.delete
+    @item.delete unless @item.user.id != current_user.id
     redirect_to root_path
-    end
   end
 
   private
