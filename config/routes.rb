fb8bbcb162ac78a1e7e7df2583da1bafa761335a
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-06 17:28:57 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-  resources :dealings , only: :index
+  resources :dealings , only: [:index,create]
 end
