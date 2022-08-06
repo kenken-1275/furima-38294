7be930c86dd72413d6f264e54e213bed9c616b9f
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-06 17:38:40 +0900

    ルーティングのネスト化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :dealings , only: [:index,create]
+  resources :items do
+    resources :dealings , only: [:index,:create]
+  end
 end
