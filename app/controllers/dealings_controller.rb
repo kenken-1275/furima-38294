b53d7d0e4a08a681bebd83a4bb546f039abbb791
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-06 18:51:25 +0900

    dealingsコントローラーにnew・createアクション定義

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,6 +1,25 @@
 class DealingsController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    @dealing_buyer = DealingBuyer.new
   end
-  
+
+  def create
+    @dealing_buyer = DealingBuyer.new(dealing_params)
+    if @dealing_buyer.valid?
+      @dealing_buyer.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def dealing_params
+    params.require(:dealing_buyer).permit(:post_number,:tel_number,:city,:address,:building_name,:area_id,).merge(item_id:params[:item_id],user_id:current_user.id)
+  end
+
 end
