bc268ae11fac2fb2832001fd4c47a4456d774323
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-06 22:10:01 +0900

    dealingsコントローラー内の記述変更

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,25 +1,42 @@
 class DealingsController < ApplicationController
+  before_action :set_item
+  before_action :authenticate_user!,only:[:index]
 
   def index
-    @item = Item.find(params[:item_id])
     @dealing_buyer = DealingBuyer.new
+    if Dealing.exists?(item_id:@item.id) || @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def create
     @dealing_buyer = DealingBuyer.new(dealing_params)
     if @dealing_buyer.valid?
+      pay_item
       @dealing_buyer.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def dealing_params
-    params.require(:dealing_buyer).permit(:post_number,:tel_number,:city,:address,:building_name,:area_id,).merge(item_id:params[:item_id],user_id:current_user.id)
+    params.require(:dealing_buyer).permit(:post_number,:tel_number,:city,:address,:building_name,:area_id,).merge(item_id:params[:item_id],user_id:current_user.id,token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: dealing_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
