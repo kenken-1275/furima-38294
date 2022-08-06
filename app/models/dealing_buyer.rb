5388653e8eab64b5cb6cfbaae65fb9e3fd3ebc99
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-06 22:15:28 +0900

    :tokenのバリデーション追加

--- app/models/dealing_buyer.rb
+++ app/models/dealing_buyer.rb
@@ -1,6 +1,6 @@
 class DealingBuyer
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id,:post_number,:area_id,:city,:address,:building_name,:tel_number,:dealing_id
+  attr_accessor :user_id,:item_id,:post_number,:area_id,:city,:address,:building_name,:tel_number,:dealing_id,:token
 
   with_options presence: true do
     validates :post_number, format:{with:/\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,15 +10,12 @@ class DealingBuyer
     validates :city
     validates :address
     validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :token
   end
   
   def save
     dealing = Dealing.create(user_id:user_id,item_id:item_id)
     Buyer.create(post_number:post_number, area_id:area_id, city:city, address:address, building_name:building_name, tel_number:tel_number, dealing_id:dealing.id)
   end
-
-
-
-  
 
 end
